4331b094ace53df16726f7863b43a7799a62f752
Alia Saddik <82057274+aliasaddik@users.noreply.github.com>
2021-12-05 10:53:44 +0200

    few changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -46,11 +46,10 @@ const userSchema = new Schema({
         required:true
     }
 ,
-Tickets:{
-  Id: [{type: mongoose.Schema.Types.ObjectId,required:true,
+TicketsId: [{type: mongoose.Schema.Types.ObjectId,required:true,
       ref:"Ticket"}]
   
-},
+,
     IsAdmin:{
         type: Boolean,
         required: true
